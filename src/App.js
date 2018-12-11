f7019ce5e652070c4eb5a10a47f6fa19f7a44331
Vedran Marsic <vedran.marsic@axilis.com>
2018-12-11 09:58:18 +0100

    Quote machine works. Everything in state.

--- src/App.js
+++ src/App.js
@@ -5,24 +5,50 @@ import axios from 'axios';
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       quotes: [{
-        quote: 'ant',
-        autor: 'bee',
+        quote: '...',
+        autor: 'unknown',
       }],
       index: 0,
       current: {
-        quote: 'ant',
-        author: 'bee',
+        quote: '...',
+        author: 'unknown',
       }
     };
+
+    this.getQuotes()
+      .then(res => this.setQuotes(res.data.quotes))
+      .then(this.nextQuote);
   }
+
+  setQuotes = (quotes) => this.setState(Object.assign(this.state, { quotes }));
 
   getQuotes = () => axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
 
-  handleClick = () => this.getQuotes()
-    .then(response => console.log(response.data))
-    .catch(err => console.error(err));
+  nextQuote = () => {
+    const randIndex = Math.floor(Math.random() * Math.floor(this.state.quotes.length));
+    
+    const randQuote = this.state.quotes[randIndex];
+    const newState = Object.assign(
+      this.state, 
+      {
+        current: {
+          ...randQuote
+        }
+      }
+    );
+    return new Promise((resolve, reject) => {
+      try {
+        this.setState(newState, () => resolve(this.state));
+      } catch(e) {
+        reject(e);
+      }
+    });
+  }
+
+  handleClick = () => this.nextQuote();
 
   render() {
     return (
