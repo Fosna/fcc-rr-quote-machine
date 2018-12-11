b187a801e0381978f208f183c6f2a486f90f4b60
Vedran Marsic <vedran.marsic@axilis.com>
2018-12-11 11:55:33 +0100

    Updates

---
+++ precache-manifest.001afd1a6c85e31641272772f8401e6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5eaafdd53989145351d5",
+    "url": "/fcc-rr-quote-machine/static/js/runtime~main.5eaafdd5.js"
+  },
+  {
+    "revision": "f197bc7a8429ff309e4d",
+    "url": "/fcc-rr-quote-machine/static/js/main.f197bc7a.chunk.js"
+  },
+  {
+    "revision": "3dda065f1320dbcde39b",
+    "url": "/fcc-rr-quote-machine/static/js/1.3dda065f.chunk.js"
+  },
+  {
+    "revision": "f197bc7a8429ff309e4d",
+    "url": "/fcc-rr-quote-machine/static/css/main.ceb6944c.chunk.css"
+  },
+  {
+    "revision": "e83d100560922ac75ad4f59dd50d17e2",
+    "url": "/fcc-rr-quote-machine/index.html"
+  }
+];
