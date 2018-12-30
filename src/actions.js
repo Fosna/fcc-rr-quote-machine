a8e2659e0bef61272a652c9572393a22b2330d9b
Vedran Marsic <vedran.marsic@axilis.com>
2018-12-30 09:43:29 +0100

    quotes url fix

--- src/actions.js
+++ src/actions.js
@@ -12,7 +12,7 @@ const createActionThunkWithType = (actionType, fn) => {
 
 export const nextQuote = createAction('NEXT_QUOTE');
 
-const getQuotes = () => axios.get('https://gistTTT.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
+const getQuotes = () => axios.get('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json');
 export const loadQuotes = createActionThunkWithType('LOAD_QUOTES', getQuotes);
 
 // Action type based on createActionThunk naming convention.
